87e5d53f2d3f2d50834bcc08b6a6c15456703d1f
dushyantbali <145493806+dushyantbali@users.noreply.github.com>
2025-03-21 11:13:08 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,27 +1,16 @@
-# mobileapp_lab3
+# flutter_application_lab3
 
-For this code I took help from my friend Mandeep and the online sources.
+A new Flutter project.
 
-This code works in the following way:
-mobile application that functions like a digital picture frame, displaying images on a rotating cycle. The app will fetch and display at least four images, automatically changing them every 10 seconds. Additionally, it will include a pause/resume button to control the image rotation. The application will support both Android and iOS platforms.
+## Getting Started
 
-Connect to AWS Cloud Storage
+This project is a starting point for a Flutter application.
 
-Store images in an S3 bucket.
-Fetch image URLs dynamically using AWS SDK.
-Create the UI (Digital Picture Frame)
+A few resources to get you started if this is your first Flutter project:
 
-Design a full-screen image viewer with a unique border.
-Use Image (React Native) or Image.network() (Flutter) for best resolution.
-Implement Image Rotation
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
 
-Store image URLs in state.
-Use setInterval() (React Native) or Timer.periodic() (Flutter) to change images every 10 seconds.
-Add Pause/Resume Button
-
-a button to toggle image rotation on/off.
-Use a boolean state variable to control the interval.
-Optimize for Performance
-
-Preload images for smooth transitions.
-Ensure a responsive layout for different screen sizes.
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
--- main.dart
+++ main.dart
@@ -1,122 +1,66 @@
-import 'package:flutter/material.dart';
+import React, { useState, useEffect } from 'react';
+import { View, Image, TouchableOpacity, Text, StyleSheet } from 'react-native';
 
-void main() {
-  runApp(const MyApp());
-}
+const images = [
+  'https://your-aws-bucket.s3.amazonaws.com/image1.jpg',
+  'https://your-aws-bucket.s3.amazonaws.com/image2.jpg',
+  'https://your-aws-bucket.s3.amazonaws.com/image3.jpg',
+  'https://your-aws-bucket.s3.amazonaws.com/image4.jpg',
+];
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+const ImageRotator = () => {
+  const [index, setIndex] = useState(0);
+  const [paused, setPaused] = useState(false);
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+  useEffect(() => {
+    if (!paused) {
+      const interval = setInterval(() => {
+        setIndex((prevIndex) => (prevIndex + 1) % images.length);
+      }, 10000);
+      return () => clearInterval(interval);
+    }
+  }, [paused]);
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  return (
+    <View style={styles.container}>
+      <View style={styles.frame}>
+        <Image source={{ uri: images[index] }} style={styles.image} />
+      </View>
+      <TouchableOpacity style={styles.button} onPress={() => setPaused(!paused)}>
+        <Text style={styles.buttonText}>{paused ? 'Resume' : 'Pause'}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f0f0f0',
+  },
+  frame: {
+    borderWidth: 10,
+    borderColor: '#8B4513', // Brown frame color
+    padding: 10,
+    borderRadius: 15,
+  },
+  image: {
+    width: 300,
+    height: 300,
+    resizeMode: 'contain',
+  },
+  button: {
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: '#007AFF',
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+});
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
+export default ImageRotator;
